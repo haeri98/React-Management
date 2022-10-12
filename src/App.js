d48f07b65cedfde6d30d19d216bdf112a5194c2e
haeri.kim <rlagofl913@gmail.com>
2022-10-12 10:38:28 +0900

    여러 고객정보 출력하기

--- src/App.js
+++ src/App.js
@@ -2,21 +2,52 @@ import logo from './logo.svg';
 import './App.css';
 import Customer from './components/Customer';
 
-const customer = {
+const customers = [
+  {
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/1',
   'name' : '홍길동',
-  'birthday' : '96 1012',
+  'birthday' : '961012',
   'gender' : '남자',
   'job' : '대학생'
-}
+  },
+  {
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '둥글래',
+  'birthday' : '970122',
+  'gender' : '여자',
+  'job' : '대학생'
+  },
+  {
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '김옥순',
+  'birthday' : '980302',
+  'gender' : '여자',
+  'job' : '회사원'
+  }
+]
 
 function App() {
   return (
-    <Customer 
-      name={customer.name}
-      birthday={customer.birthday}
-      gender={customer.gender}
-      job={customer.job}
-    />
+    <div>
+    {
+      customers.map(c => {
+        return (
+          <Customer
+            key={c.id}
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}
+          />
+        );
+      })
+    }
+    </div>
   );
 }
 
